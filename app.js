948589970b9282650299cf0440e6fb149889f3f3
habibrehman001 <khuwajaabdulrehman046@gmail.com>
2026-01-16 23:15:32 +0500

    cal

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ let calculate = () => {
   let inpt = document.querySelector('#inpt');
   let expression = inpt.value;
 
-  // handle multiplication symbol
-  expression = expression.replace(/x/g, '*');
-
   try {
     let result = eval(expression);
     inpt.value = result;
